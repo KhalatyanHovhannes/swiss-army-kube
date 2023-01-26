436c7d3b83077f5e749fad66c26755bd01cbd872
Hovhannes Khalatyan <Hovhannes.Khalatyan.Contractor@pepsico.com>
2023-01-26 10:20:54 +0400

    nginx

--- examples/argocd/main.tf
+++ examples/argocd/main.tf
@@ -5,19 +5,9 @@ data "aws_eks_cluster" "cluster" {
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
 }
-data "aws_route53_zone" "this" {
-  # name         = "edu.provectus.io."
-  zone_id      = var.zone_id
-  private_zone = false
-}
-
 data "aws_availability_zones" "available" {}
 
 locals {
-  #environment  = var.environment
-  #project      = var.project
-  #cluster_name = var.cluster_name
-  #domain       = ["${local.cluster_name}.${var.domain_name}"]
   tags = {
     environment = local.environment
     project     = local.project
@@ -175,39 +165,15 @@ module "argocd" {
     "server.ingress.paths[0]" = "/"
   }
 }
-module "external_dns" {
-  depends_on = [module.argocd]
-
-  source       = "github.com/provectus/sak-external-dns"
-  cluster_name = local.cluster_name
-  argocd       = module.argocd.state
-  mainzoneid   = data.aws_route53_zone.this.zone_id
-  hostedzones  = local.domain
-  tags         = local.tags
-}
-module "clusterwide" {
-  depends_on = [module.argocd]
-  source     = "terraform-aws-modules/acm/aws"
-  version    = "~> v2.12"
-
-  domain_name = "*.${local.domain[0]}"
-  subject_alternative_names = [
-    local.domain[0]
-  ]
-  zone_id              = module.external_dns.zone_id
-  validate_certificate = true #Disable if used private DNS and validate it manually
-  wait_for_validation  = false
-  tags                 = local.tags
-}
 module "nginx-ingress" {
-  depends_on   = [module.clusterwide]
+  #depends_on   = [module.clusterwide]
   source       = "github.com/provectus/sak-nginx"
   #cluster_name = module.kubernetes.cluster_name
   argocd       = module.argocd.state
   conf = {
     "controller.service.targetPorts.http"                                                                = "http"
     "controller.service.targetPorts.https"                                                               = "http"
-    "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-cert"         = module.clusterwide.this_acm_certificate_arn
+    "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-cert"         = "arn:aws:acm:eu-north-1:641456973426:certificate/595ae84a-0019-43ff-b540-d2af938fd523" #module.clusterwide.this_acm_certificate_arn
     "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-backend-protocol" = "http"
     "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-ports"        = "https"
   }
