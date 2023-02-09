6e0a69853877ca8d0ff7ea6054f13be1178ca89f
Hovhannes Khalatyan <Hovhannes.Khalatyan.Contractor@pepsico.com>
2023-02-09 14:24:25 +0400

    added new files

--- examples/argocd/main.tf
+++ examples/argocd/main.tf
@@ -214,3 +214,14 @@ module "alb-ingress" {
   vpc_id       = module.vpc.vpc_id
   argocd       = module.argocd.state
 }
+module "cert-manager" {
+  depends_on   = [module.argocd]
+
+  source       = "/Users/hovhannes/Documents/Work/Provectus/sak-cert-manager"
+  cluster_name = module.eks.cluster_id
+  argocd       = module.argocd.state
+  email        = "your@email.com"
+  zone_id      = data.aws_route53_zone.this.zone_id
+  vpc_id       = module.vpc.vpc_id
+  domains      = local.domain
+}
